fabffea60759272e599c84bf84ebd4962de33192
Zulay Scottborgh <zulayscottborgh@Zulays-MBP.lan>
2020-09-04 13:37:32 -0400

    updated landing jumbotron media queries

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,10 +4,12 @@ import '../styling/Landing.css';
 export default function Landing() {
   return (
     <div>
-      <div className="jumbo-container">
-        <h1 className="title display-3">Zulay Scottborgh</h1>
-        <hr className="my-2 line" />
-        <p className="lead">
+      <div className="jumbotron mx-2 mb-2">
+        <h1 className="card-title display-3  h1 my-4 pb-4 col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          Zulay Scottborgh
+        </h1>
+        <hr className="my-4 mb-2 line" />
+        <p className="subtitle">
           Software Engineer | Web Developer | Digital Marketing
         </p>
       </div>
